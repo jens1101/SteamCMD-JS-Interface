e18c22e6640f076b0b8c17d5643226d59148c40a
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-04-20 15:14:00 +0200

    WIP adding more tests

--- tests/steam-cmd.test.js
+++ tests/steam-cmd.test.js
@@ -3,9 +3,33 @@ import { SteamCmd } from '../dist/steam-cmd.cjs'
 let steamCmd
 
 global.beforeAll(async () => {
-  steamCmd = await SteamCmd.init({})
+  steamCmd = await SteamCmd.init()
 })
 
 global.test('Can login anonymously', async () => {
   await steamCmd.login('anonymous')
+})
+
+global.describe('Test supported platforms', () => {
+  let originalPlatformDescriptor
+
+  global.beforeAll(() => {
+    // Get the original property descriptor for `process.platform`
+    originalPlatformDescriptor = Object.getOwnPropertyDescriptor(process,
+      'platform')
+  })
+
+  global.afterAll(() => {
+    // Restore `process.platform` to its original value
+    Object.defineProperty(process, 'platform', originalPlatformDescriptor)
+  })
+
+  global.test('Initializes on all supported platforms', async () => {
+    const platforms = ['win32', 'darwin', 'linux']
+
+    for (const platform of platforms) {
+      Object.defineProperty(process, 'platform', { value: platform })
+      await SteamCmd.init()
+    }
+  }, 15000)
 })
